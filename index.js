25e1e6f616810c939247772facae5b6a0abbb83d
Magno <magno.alefb@gmail.com>
2024-01-12 08:47:41 -0300

    ts

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
-var setPilot = function (newPilot, spaceship) {
-    spaceship.pilot = newPilot;
-};
-var accelerate = function (targetSpeed, spaceship) {
-    spaceship.speed = targetSpeed;
-};
-var sendToMission = function (spaceship) {
-    spaceship.inMission = true;
-};
-var spaceship = {
-    name: '',
-    pilot: '',
-    speed: 0,
-    inMission: false,
-};
-var pilot = "Han Solo";
-spaceship.name = "Millennium Falcon";
-setPilot(pilot, spaceship);
-accelerate(50, spaceship);
-sendToMission(spaceship);
-console.log(spaceship);
-//tsc index.ts comando para trasnforma esse arquivo em um arquivo js
+// function sendSpaceship(name, captain) {
+//     var spaceship = {
+//         name: name,
+//         captain: captain,
+//         speed: 20,
+//         inMission: true,
+//         crew: []
+//     };
+//     alert("A nave ".concat(spaceship.name, " comandada pelo capit\u00E3o ").concat(spaceship.captain, " foi enviada em uma miss\u00E3o."));
+//     return spaceship;
+// }
+// function accelerate(targetSpeed, spaceship) {
+//     if (spaceship.speed > targetSpeed) {
+//         alert("Reduzindo a velocidade da ".concat(spaceship.name, " para ").concat(targetSpeed, "..."));
+//     }
+//     else if (spaceship.speed < targetSpeed) {
+//         alert("Aumentando a velocidade da ".concat(spaceship.name, " para ").concat(targetSpeed, "..."));
+//     }
+//     else {
+//         alert("Mantendo a velocidade da ".concat(spaceship.name, "..."));
+//     }
+// }
+// var spaceshipName = prompt('Insira o nome da nave a ser enviada:');
+// var spaceshipCaptain = prompt('Insira o nome do capitão da nave:');
+// sendSpaceship(spaceshipName, spaceshipCaptain);
+// var currentShip = sendSpaceship(spaceshipName, spaceshipCaptain);
+// var speed = Number(prompt('Insira a velocidade para a qual deseja acelerar:'));
+// accelerate(speed, currentShip);
+// accelerate(speed, currentShip);
